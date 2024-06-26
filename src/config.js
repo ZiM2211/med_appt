c8178d5a7c328e0724478f46bc9d81ea3fb85156
ZiM2211 <i.gritsayuk@gmail.com>
2024-06-26 07:53:56 -0400

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+export const API_URL = window.location.hostname === "localhost" ? "https://igritsayuk-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "add your theia server sidhttps://igritsayuk-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/e url";
 console.log(
-    "API_URL :",
+    "https://igritsayuk-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/",
     API_URL
 );
